b31a6bc412b17306cb81c6025124c0406bf4a2d5
Tanmaya Mahapatra <tanmaya.mahapatra@tum.de>
2019-06-20 15:26:07 +0200

    Licence Changes

--- asyncFlow/java/aFlux/src/main/java/de/tum/in/aflux/dao/FlowSettingRepository.java
+++ asyncFlow/java/aFlux/src/main/java/de/tum/in/aflux/dao/FlowSettingRepository.java
@@ -1,24 +1,20 @@
+
+
 /*
+ * aFlux: JVM based IoT Mashup Tool
+ * Copyright [2019] [Tanmaya Mahapatra]
  *
- *  *
- *  * aFlux: JVM based IoT Mashup Tool
- *  * Copyright (C) 2018  Tanmaya Mahapatra
- *  *
- *  * This file is part of aFlux.
- *  *
- *  * aFlux is free software: you can redistribute it and/or modify
- *  * it under the terms of the GNU General Public License as published by
- *  * the Free Software Foundation, version 3 of the License.
- *  *
- *  * aFlux is distributed in the hope that it will be useful,
- *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  * GNU General Public License for more details.
- *  *
- *  * You should have received a copy of the GNU General Public License
- *  * along with aFlux.  If not, see <http://www.gnu.org/licenses/>.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package de.tum.in.aflux.dao;
